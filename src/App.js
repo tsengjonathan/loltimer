8706ca9c559ef2bcc29411d1429c4bbc07472b0a
tsengjonathan <tseng.j@husky.neu.edu>
2020-05-31 00:40:21 -0400

    Build initial layout for page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import TimerChip from './components/TimerChip';
+import Champion from './components/Champion';
 
 function App() {
   return (
-    <div className="App">
-      <TimerChip duration={15} />
+    <div className="container">
+      <div className="column">
+        <Champion id="Jax" />
+        <Champion id="LeeSin" />
+        <Champion id="Taliyah" />
+        <Champion id="Caitlyn" />
+        <Champion id="Thresh" />
+      </div>
+      <div className="column">
+        <Champion id="Malphite" />
+        <Champion id="Kayn" />
+        <Champion id="Orianna" />
+        <Champion id="Ezreal" />
+        <Champion id="Leona" />
+      </div>
     </div>
   );
 }
